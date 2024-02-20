455a4e782efd281ff2b144036e24d3957ed440ed
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-20 16:33:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class DiceMosaicApp:
             fixed1 = remove(image1)
             buf = BytesIO()
             fixed1.save(buf, format="PNG")
+            self.image_path1=fixed1
             st.image(fixed1, caption="Selected Image 1", use_column_width=True)
 
         self.image_path2 = st.file_uploader("Select Image 2:", type=["png", "jpg", "jpeg"])
@@ -37,6 +38,7 @@ class DiceMosaicApp:
             fixed2 = remove(image2)
             buf = BytesIO()
             fixed2.save(buf, format="PNG")
+            self.image_path2=fixed2
             st.image(fixed2, caption="Selected Image 2", use_column_width=True)
             
         if st.button("Convert to Mosaic"):
@@ -47,8 +49,8 @@ class DiceMosaicApp:
             numDiceWide = 100
             numDiceTall = 100
 
-            source_image1 = Image.open(fixed1)
-            source_image2 = Image.open(fixed2)
+            source_image1 = Image.open(self.image_path1)
+            source_image2 = Image.open(self.image_path2)
             die_one = Image.open("DiceImages/1.png")
             die_two = Image.open("DiceImages/2.png")
             die_three = Image.open("DiceImages/3.png")
