5278079cdc0e2435ea4bc9d9e426e25b921b73a9
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-20 16:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ class DiceMosaicApp:
             image1 = Image.open(self.image_path1)
             fixed1 = remove(image1)
             buf = BytesIO()
-            self.image_1=fixed1.save(buf, format="PNG")
+            fixed1.save(buf, format="PNG")
+            self.image_1=fixed1
             st.image(fixed1, caption="Selected Image 1", use_column_width=True)
 
         self.image_path2 = st.file_uploader("Select Image 2:", type=["png", "jpg", "jpeg"])
@@ -38,7 +39,8 @@ class DiceMosaicApp:
             image2 = Image.open(self.image_path2)
             fixed2 = remove(image2)
             buf = BytesIO()
-            self.image_2=fixed2.save(buf, format="PNG")
+            fixed2.save(buf, format="PNG")
+            self.image_2=fixed2
             st.image(fixed2, caption="Selected Image 2", use_column_width=True)
             
         if st.button("Convert to Mosaic"):
