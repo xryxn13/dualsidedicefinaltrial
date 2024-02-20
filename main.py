d4602c770178ac0066441eb88f1ea6d99347d8e6
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-20 19:18:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,14 +87,6 @@ class DiceMosaicApp:
             
         if st.button("Convert to Mosaic",type='primary'):
             self.convert_to_mosaic()
-
-    col1, col2 = st.columns(2)
-    with col1:
-       st.header("A cat")
-       st.image("https://static.streamlit.io/examples/cat.jpg")
-    with col2:
-       st.header("A cat")
-       st.image("https://static.streamlit.io/examples/cat.jpg")
     
     def convert_to_mosaic(self):
         if self.image_path1 and self.image_path2:
